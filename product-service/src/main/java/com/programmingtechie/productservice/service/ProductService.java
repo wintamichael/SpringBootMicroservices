2a9c86577c38b2d539f80691d4a9ec12f4c61676
wintamichael <wintam951@gmail.com>
2023-09-02 12:13:02 -0500

    first commit

--- product-service/src/main/java/com/programmingtechie/productservice/service/ProductService.java
+++ product-service/src/main/java/com/programmingtechie/productservice/service/ProductService.java
@@ -1,11 +1,14 @@
 package com.programmingtechie.productservice.service;
 
 import com.programmingtechie.productservice.dto.ProductRequest;
+import com.programmingtechie.productservice.dto.ProductResponse;
 import com.programmingtechie.productservice.model.Product;
 import com.programmingtechie.productservice.repository.ProductRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -22,4 +25,19 @@ public class ProductService {
         log.info("product {} is saved", product.getId());
 
     }
+
+    public List<ProductResponse> getAllProducts() {
+       List<Product> products= productRepository.findAll();
+        return products.stream().map(this::mapToProductResponse).toList();
+    }
+
+    private ProductResponse mapToProductResponse(Product product) {
+        return ProductResponse.builder()
+                .id(product.getId())
+                .name(product.getName())
+                .description(product.getDescription())
+                .price(product.getPrice())
+                .build();
+    }
+
 }
